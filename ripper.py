9e2ae509e195083a74871bc6979ea8f7ffcb1eef
John Hodson <hodsonus@gmail.com>
2019-04-25 10:12:47 -0400

    add print statements and reduce sleep time

--- ripper.py
+++ ripper.py
@@ -1,10 +1,9 @@
 import mechanize
-from bs4 import BeautifulSoup
 import time
 import re
 import pandas as pd
-from pandas import ExcelWriter
 import config
+import bs4
 
 username = config.credentials['yahoo_email']
 password = config.credentials['yahoo_password']
@@ -16,30 +15,34 @@ br = mechanize.Browser()
 br.set_handle_robots(False)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.6')]
 
+print('Logging in.')
 url = 'https://login.yahoo.com/config/login;_ylt=AwrGB7pqzTxUF9oAigC3cJ8u?.src=fantasy&.intl=us&.lang=en-US&.done=http://baseball.fantasysports.yahoo.com/b1/'+str(league_id)+'/players'
 br.open(url)
-time.sleep(1)
+time.sleep(.5)
 
 br.select_form(nr=0)
 br.form["username"] = username
 br.form["passwd"] = password
 response = br.submit()
-time.sleep(1)
+time.sleep(.5)
 
 br.select_form(nr = 0)
 br.form["password"] = password
 response = br.submit()
-time.sleep(1)
+time.sleep(.5)
+print('Logged in!')
 
-# i=0
+i=1
 while (True): # rip player data until we hit the break statement
 
     html_scrape = response.read()
-    soup = BeautifulSoup(html_scrape, "lxml")
+    soup = bs4.BeautifulSoup(html_scrape, "lxml")
 
     # with open("out"+str(i)+".html", "w") as text_file:
     #     text_file.write(str(soup))
-    # i+=1
+
+    print('Processing page ',i,'.',sep="")
+    i+=1
 
     players = soup.find_all('div', {'class':'ysf-player-name Nowrap Grid-u Relative Lh-xs Ta-start'})
     for player in players:
@@ -55,11 +58,15 @@ while (True): # rip player data until we hit the break statement
     if (link == None):
             break
     response = br.follow_link(link)
-    time.sleep(1)
+    time.sleep(.5)
 
+print('Creating dataframe.')
 df = pd.DataFrame(player_info, columns=['PLAYER', 'TEAM', 'POS'])
 
-print(df)
-writer = ExcelWriter('Players.xlsx')
+# print(df)
+
+print('Writing the dataframe to an excel spreadsheet.')
+writer = pd.ExcelWriter('Players.xlsx')
 df.to_excel(writer,'Sheet',index=False)
 writer.save()
+print('Done! Check the working directory for the completed spreadsheet.')
