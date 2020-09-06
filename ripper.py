69120ee70ee8ec8f671495368bee05cddec1efda
John Hodson <hodsonus@gmail.com>
2020-09-06 13:53:40 -0700

    misc changes i have to sort through and resolve merge conflicts with

--- ripper.py
+++ ripper.py
@@ -11,6 +11,8 @@ league_id = config.credentials['yahoo_league_id']
 
 player_info = []
 
+sleep_time = 0.25
+
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.6')]
@@ -18,18 +20,18 @@ br.addheaders = [('User-agent', 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US;
 print('Logging in.')
 url = 'https://login.yahoo.com/config/login;_ylt=AwrGB7pqzTxUF9oAigC3cJ8u?.src=fantasy&.intl=us&.lang=en-US&.done=http://baseball.fantasysports.yahoo.com/b1/'+str(league_id)+'/players'
 br.open(url)
-time.sleep(.5)
+time.sleep(sleep_time)
 
 br.select_form(nr=0)
 br.form["username"] = username
 br.form["passwd"] = password
 response = br.submit()
-time.sleep(.5)
+time.sleep(sleep_time)
 
 br.select_form(nr = 0)
 br.form["password"] = password
 response = br.submit()
-time.sleep(.5)
+time.sleep(sleep_time)
 print('Logged in!')
 
 # Rip batters
@@ -49,7 +51,9 @@ while (True): # rip player data until we hit the break statement
     players = soup.find_all('div', {'class':'ysf-player-name Nowrap Grid-u Relative Lh-xs Ta-start'})
     for player in players:
         name = player.find('a').get_text()
-        team_pos_str = player.find('span').get_text()
+        player_span = player.find('span')
+        if (player_span == None): pass
+        team_pos_str = player_span.get_text()
         team_pos_lis = re.split(' - ',team_pos_str)
         team = team_pos_lis[0]
         pos = team_pos_lis[1]
@@ -60,12 +64,12 @@ while (True): # rip player data until we hit the break statement
     if (link == None):
             break
     response = br.follow_link(link)
-    time.sleep(.5)
+    time.sleep(sleep_time)
 
 # Rip pitchers
 newurl = 'https://baseball.fantasysports.yahoo.com/b1/'+str(league_id)+'/players?&sort=OR&sdir=1&status=A&pos=P&stat1=S_S_2019&jsenabled=1'
 br.open(newurl)
-time.sleep(.5)
+time.sleep(sleep_time)
 
 i=1
 while (True): # rip player data until we hit the break statement
@@ -93,7 +97,7 @@ while (True): # rip player data until we hit the break statement
     if (link == None):
             break
     response = br.follow_link(link)
-    time.sleep(.5)
+    time.sleep(sleep_time)
 
 print('Creating dataframe.')
 df = pd.DataFrame(player_info, columns=['PLAYER', 'TEAM', 'POS'])
